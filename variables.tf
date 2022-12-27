1edce94017ac49697bb810cf0904b5220a1bcdfc
jmeisele <jmeisele@yahoo.com>
2022-12-27 19:40:04 +0000

    :construction_worker:

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "account_id" {
+  type        = number
+  description = "AWS Account ID"
+  default     = 288195736164
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
