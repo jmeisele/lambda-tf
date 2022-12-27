5a50c979cebba88eb04a6735e443070c23c7a921
jmeisele <jmeisele@yahoo.com>
2022-12-27 21:23:56 +0000

    :construction_worker:

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -17,8 +17,10 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-# IAM policy for logging from a lambda and getting our image from ECR
+
+# IAM policy for logging from a lambda and getting our image from ECR, depends on if we need access to dynamoDB
 resource "aws_iam_policy" "iam_policy_for_lambda" {
+  count       = var.access_to_dynamo_db ? 0 : 1
   name        = "aws_iam_policy_for_terraform_aws_lambda_role"
   path        = "/"
   description = "AWS IAM Policy for managing aws lambda role"
@@ -26,7 +28,7 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
 {
   "Version": "2012-10-17",
   "Statement": [
-    {
+    {mages
       "Effect": "Allow",
       "Action": [
         "logs:CreateLogGroup",
@@ -48,7 +50,44 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
         "dynamodb:Query",
         "dynamodb:Scan"
       ],
-      ${var.access_to_dynamo_db ? "Resource": "arn:aws:dynamodb:${var.region}:${var.account_id}:table/${var.dynamo_db_table_name}"}
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda_dynamo" {
+  count       = var.access_to_dynamo_db ? 1 : 0
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {mages
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": "ecr:BatchGetImage",
+      "Resource": "arn:aws:ecr:${var.region}:${var.account_id}:${var.ecr_name}/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:BatchGetItem",
+        "dynamodb:GetItem",
+        "dynamodb:Query",
+        "dynamodb:Scan"
+      ],
+      "Resource": "arn:aws:dynamodb:${var.region}:${var.account_id}:table/${var.dynamo_db_table_name}"}
     }
   ]
 }
@@ -58,7 +97,7 @@ EOF
 # Policy Attachment on the role.
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+  policy_arn = var.access_to_dynamo_db ? aws_iam_policy.iam_policy_for_lambda.arn : aws_iam_policy.iam_policy_for_lambda_dynamo.arn
 }
 
 data "aws_ecr_repository" "repository" {
