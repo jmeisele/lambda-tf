5757f9680c632e516249d79dafef3789aded26a0
jmeisele <jmeisele@yahoo.com>
2024-07-12 11:46:18 -0500

    :rocket:

--- main.tf
+++ main.tf
@@ -23,52 +23,52 @@ provider "aws" {
   skip_metadata_api_check = true
 }
 
-# resource "aws_ecr_repository" "ecr" {
-#   name                 = "davinci_ecr"
-#   image_tag_mutability = "MUTABLE"
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
+resource "aws_ecr_repository" "ecr" {
+  name                 = "davinci_ecr"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
 
-# resource "aws_ecr_repository_policy" "ecr_policy" {
-#   repository = aws_ecr_repository.ecr.name
-#   policy     = <<EOF
-#   {
-#     "Version": "2008-10-17",
-#     "Statement": [
-#       {
-#         "Sid": "Adds access to push and pull images",
-#         "Effect": "Allow",
-#         "Principal": "*",
-#         "Action": [
-#           "ecr:BatchCheckLayerAvailability",
-#           "ecr:BatchGetImage",
-#           "ecr:CompleteLayerUpload",
-#           "ecr:GetDownloadUrlForLayer",
-#           "ecr:GetLifecyclePolicy",
-#           "ecr:InitiateLayerUpload",
-#           "ecr:PutImage",
-#           "ecr:UploadLayerPart"
-#         ]
-#       }
-#     ]
-#   }
-#   EOF
-# }
+resource "aws_ecr_repository_policy" "ecr_policy" {
+  repository = aws_ecr_repository.ecr.name
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "Adds access to push and pull images",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  }
+  EOF
+}
 
-# module "lambda_func" {
-#   source        = "./modules/lambda"
-#   name          = "lambda_func"
-#   ecr_name      = aws_ecr_repository.ecr.name
-#   ecr_image_tag = "lambda_func"
-# }
+module "lambda_func" {
+  source        = "./modules/lambda"
+  name          = "lambda_func"
+  ecr_name      = aws_ecr_repository.ecr.name
+  ecr_image_tag = "lambda_func"
+}
 
-# module "lambda_func_api_gateway" {
-#   source                 = "./modules/api_gateway"
-#   name                   = "lambda_func_api_gateway"
-#   lambda_func_name       = module.lambda_func.name
-#   lambda_func_invoke_arn = module.lambda_func.invoke_arn
-#   stage_name             = "api"
-#   endpoint               = "customers"
-# }
+module "lambda_func_api_gateway" {
+  source                 = "./modules/api_gateway"
+  name                   = "lambda_func_api_gateway"
+  lambda_func_name       = module.lambda_func.name
+  lambda_func_invoke_arn = module.lambda_func.invoke_arn
+  stage_name             = "api"
+  endpoint               = "customers"
+}
